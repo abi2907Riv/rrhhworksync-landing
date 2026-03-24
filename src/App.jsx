d0f043fe74964fd1873ba27aeafa69efa5c476f9
AbiRivadero <abirivadero@gmail.com>
2026-03-24 20:58:56 -0300

    feat: initial commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from './assets/vite.svg'
+import heroImg from './assets/hero.png'
+
+function App() {
+  return (
+    <h1 className="text-3xl font-bold text-blue-500">
+      Tailwind funcionando 🚀
+    </h1>
+  );
+}
+
+export default App
